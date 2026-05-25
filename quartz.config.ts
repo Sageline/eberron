c1a95c0522683f2ebab2758bec49d215698b1876
Sageline <martinsageline@gmail.com>
2026-05-25 19:33:12 -0300

    Quartz sync: May 25, 2026, 7:33 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "es-ES",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "dm", "DM"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
